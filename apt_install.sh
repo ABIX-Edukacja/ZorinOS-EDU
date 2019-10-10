e7f0c9ca5a856dc7e161654902f107aa231f7456
ABIX Edukacja Adam Jurkiewicz <a.jurkiewicz@abix.info.pl>
2019-10-10 19:53:42 +0200

    Initial scripts

---
+++ apt_install.sh
@@ -0,0 +1,21 @@
+apt install python3-pip mc openvpn
+apt install thunderbird-locale-pl thunderbird-gnome-support
+apt install chromium-browser*
+apt install libqt4-dev-bin
+apt install libcanberra-gtk3-module
+apt install libcanberra-gtk0
+apt install git
+apt install ar
+apt install dpkg
+apt install python3-idle
+apt install idle-python3.6
+apt install idle-python3.7
+apt install dosemu
+apt install mp3check cdparanoia aac-enc faac
+apt install cd-discid
+apt install python3-matplotlib
+apt install python3-dev
+apt install python-dev
+apt install hplip-data
+apt install hplip-gui
+apt --fix-broken install
---
+++ pip3_install.sh
@@ -0,0 +1,5 @@
+pip3 install pgzero
+pip3 install mu-editor
+pip3 install matplotlib
+pip3 install pytechbrain
+pip3 install matplotlib -U
