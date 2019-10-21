f3150ead89e5369006bb3528d4b32995dcc9761d
ABIX Edukacja Adam Jurkiewicz <a.jurkiewicz@abix.info.pl>
2019-10-21 12:12:47 +0200

    dorbne poprawki

--- apt_install.sh
+++ apt_install.sh
@@ -7,10 +7,8 @@ apt-get update
 apt install -y python3-pip mc openvpn
 apt install -y thunderbird-locale-pl thunderbird-gnome-support
 apt install -y chromium-browser*
-apt install -y libqt4-dev-bin
-apt install -y libcanberra-gtk3-module
-apt install -y libcanberra-gtk0 git ar dpkg python3-idle idle-python3.6 idle-python3.7
-apt install -y dosemu mp3check cdparanoia aac-enc faac cd-discid python3-matplotlib
+apt install -y libqt4-dev-bin libcanberra-gtk3-module libcanberra-gtk0 git ar dpkg idle-python3.6 
+apt install -y dosemu mp3check cdparanoia aac-enc faac cd-discid 
 apt install -y python3-dev python-dev hplip-data hplip-gui
 
 
