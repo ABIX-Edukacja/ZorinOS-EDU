499b50087d394ef33f492b62cc3377e1f1e78ed5
Adam Jurkiewicz <a.jurkiewicz@abix.info.pl>
2019-11-17 18:27:11 +0100

    aktualizacja 17.11.2019

--- apt_install.sh
+++ apt_install.sh
@@ -9,7 +9,7 @@ apt install -y thunderbird-locale-pl thunderbird-gnome-support
 apt install -y chromium-browser*
 apt install -y libqt4-dev-bin libcanberra-gtk3-module libcanberra-gtk0 git ar dpkg idle-python3.6
 apt install -y dosemu mp3check cdparanoia aac-enc faac cd-discid
-apt install -y python3-dev python-dev hplip-data hplip-gui
+apt install -y python3-dev python-dev hplip-data hplip-gui vim-python-jedi
 # ustawienie language
 apt-get remove -y language-pack-ar* language-pack-de* language-pack-es* language-pack-fr* language-pack-it* language-pack-ja* language-pack-pt* language-pack-ru* language-pack-zh*
 apt-get remove -y language-pack-gnome-ar* language-pack-gnome-de* language-pack-gnome-es* language-pack-gnome-fr* language-pack-gnome-it* language-pack-gnome-ja* language-pack-gnome-pt* language-pack-gnome-ru* language-pack-gnome-zh*
@@ -46,5 +46,3 @@ tar xzvf "${D}/_ab.tgz"
 chown root.root angrybirds -R
 chmod +r angrybirds -R
 
-# aktualizacja całego systemu - pytanie do usera....
-apt-get upgrade
